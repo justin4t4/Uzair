ae672aafe715d7b92eff15cb193110fcd177f83f
hp <hp@UZAIR>
2022-12-21 10:54:40 +0500

    Add project files.

---
+++ Final_Web_Proj/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Final_Web_Proj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
